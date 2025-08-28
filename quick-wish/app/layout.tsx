9449b745403c5f084b726cbd1df3ef12d92a7057
shiva kushwah <shivakushwah144@gmail.com>
2025-08-28 20:04:44 +0530

    remove clerk

--- quick-wish/app/layout.tsx
+++ quick-wish/app/layout.tsx
@@ -1,13 +1,13 @@
 
 import "./globals.css";
-import { ClerkProvider } from '@clerk/nextjs'
+
 
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
   return (
-   <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+  
       <html lang="en">
         <body>{children}</body>
       </html>
-    </ClerkProvider>
+
   );
 }
